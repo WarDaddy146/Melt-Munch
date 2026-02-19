43afc68f8ced7d6fff3872d8899b42e0016d368f
WarDaddy146 <aliakbarnashit146@gmail.com>
2026-02-19 22:54:50 +0530

    Added Design Guide

--- app/models.py
+++ app/models.py
@@ -11,7 +11,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    quantity = models.PositiveIntegerField(default=0)
+    quantity = models.PositiveIntegerField(default=1)
     category = models.CharField(max_length=100, choices=[(cat, cat) for cat in cat_ops[0]], default='Cookies')
     image = models.ImageField(upload_to='products/', blank=True, null=True)
     
--- app/views.py
+++ app/views.py
@@ -160,8 +160,7 @@ def add_to_cart(request, product_id):
     if not created:
         cart_item.quantity += 1
         cart_item.save()
-    
-    return redirect('cart')
+    return redirect('dash')
 
 @login_required(login_url='login')
 def update_cart(request, item_id):
